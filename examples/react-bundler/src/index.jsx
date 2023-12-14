abd4e67ec9f966da4d1b4cd2923768a9d35c512c
Margolin96 <margolin96@gmail.com>
2023-12-14 13:02:56 +0400

    Styles

--- examples/react-bundler/src/index.jsx
+++ examples/react-bundler/src/index.jsx
@@ -17,21 +17,23 @@ function App() {
     },
   })
   return (
-    <div>
-      <h1>VAD Demo</h1>
-      <button style={{ margin: 10 }} onClick={vad.toggle}>Toggle VAD</button>
-      {vad.listening ? <div style={{ padding: 10 }}>VAD is running</div> : <div>VAD is NOT running</div>}
-      {vad.userSpeaking && <UserSpeaking />}
-      {!vad.userSpeaking && <UserNotSpeaking />}
-      <ol id="playlist">
-        {audioList.map((audioURL) => {
-          return (
-            <li key={audioURL.substring(-10)} style={{ padding: 10 }}>
-              <audio controls src={audioURL} />
-            </li>
-          )
-        })}
-      </ol>
+    <div style={{ maxWidth: 500, margin: '0 auto', padding: 10 }}>
+      <div style={{ background: '#FFF', boxShadow: '0 2px 5px 0 rgba(0,0,0,.1)' }}>
+        <h1>VAD Demo</h1>
+        <button style={{ margin: 10 }} onClick={vad.toggle}>Toggle VAD</button>
+        <div style={{ padding: 10 }}>{vad.listening ? 'VAD is RUNNING' : 'VAD is STOPPED'}</div>
+        {vad.userSpeaking && <UserSpeaking />}
+        {!vad.userSpeaking && <UserNotSpeaking />}
+        <ol id="playlist">
+          {audioList.map((audioURL) => {
+            return (
+              <li key={audioURL.substring(-10)}>
+                <audio controls src={audioURL} />
+              </li>
+            )
+          })}
+        </ol>
+      </div>
     </div>
   )
 }
